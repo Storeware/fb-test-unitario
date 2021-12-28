46373fc7c17a1cc82c59f0bfdb86b7b8d7228489
amarildolacerAda <amarildo_51@msn.com>
2021-12-28 13:29:59 -0300

    = rename

--- rodar.bat
+++ rodar.bat
@@ -1,2 +1,2 @@
 echo rodar o teste
-isql localhost/3050:c:\dados\store.fdb -user sysdba -password masterkey -i script-9.00.001-tests-basicos.sql 
+isql localhost/3050:c:\dados\store.fdb -user sysdba -password masterkey -i script-9.00.001-tests-console.sql 
